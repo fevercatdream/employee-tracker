81b23debb3ad7d75d09ffbc9fa2a698d9124e46d
fevercatdream <kuma18@uw.edu>
2023-04-30 21:56:43 -0700

    updated query file

--- query.js
+++ query.js
@@ -1,11 +1,14 @@
 // function to view all employees
 function viewAllEmployees() {
-    db.query(`SELECT id, first_name, last_name, title, department, salary, manager
+    db.query(`SELECT employee.id, employee.first_name, employee.last_name, title, department.name AS department, salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
     FROM employee
     JOIN role
     ON employee.role_id = role.id
     JOIN department
-    ON role.department_id = department.id;`, function (err, results) {
+    ON role.department_id = department.id
+    LEFT JOIN employee AS manager
+    ON employee.manager_id = manager.id
+    ORDER BY employee.id;`, function (err, results) {
         if (err) {
             console.log(err);
         }
